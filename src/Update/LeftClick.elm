24764d1e4d2c76070ce47b65966685440204fa16
Fabian Petersen <fabian@nf-petersen.de>
2022-08-16 12:38:10 +0200

    add placing a new tower with leftClick if it can be placed

--- src/Update/LeftClick.elm
+++ src/Update/LeftClick.elm
@@ -6,4 +6,15 @@ import Point exposing (Point)
 
 update : Maybe Point -> Model -> ( Model, Cmd msg )
 update point model =
-    ( { model | clicked = point }, Cmd.none )
+    ( case model.placingTower of
+        Nothing ->
+            { model | clicked = point }
+
+        Just placingTower ->
+            if placingTower.canBePlaced then
+                { model | clicked = point, placingTower = Nothing, towers = placingTower.tower :: model.towers, money = model.money - placingTower.tower.price }
+
+            else
+                { model | clicked = point }
+    , Cmd.none
+    )
