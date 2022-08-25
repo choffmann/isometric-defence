a004f608e8bd786f0f1b6dcda2f04472ae30ad51
Fabian Petersen <fabian@nf-petersen.de>
2022-08-25 12:02:06 +0200

    finally fixed the last bit of the pathgeneration

--- src/Update/GeneratePath.elm
+++ src/Update/GeneratePath.elm
@@ -4,99 +4,59 @@ import Area
 import List.Extra as List
 import Messages exposing (Msg(..))
 import Model exposing (GameState(..), Model)
-import Path exposing (Path, PathDirection(..), PathPoint)
+import Path exposing (PathDirection(..), PathPoint)
 import Point exposing (Point)
 import Random
 
 
-checkDirection : Path -> List PathDirection
-checkDirection path =
+checkDirection : PathPoint -> List PathDirection
+checkDirection { direction, point } =
     let
-        checkDown direction y =
-            case direction of
-                Up ->
-                    False
+        checkUp p =
+            (p.y - 2) > 0
 
-                Right ->
-                    (y + 2) < Area.heightTiles
-
-                Down ->
-                    (y + 2) < Area.heightTiles
-
-        checkUp direction y =
-            case direction of
-                Up ->
-                    (y - 2) > 0
-
-                Right ->
-                    (y - 2) > 0
-
-                Down ->
-                    False
-
-        internal { direction, point } =
-            case direction of
-                Right ->
-                    if checkDown direction point.y && checkUp direction point.y then
-                        [ Right, Up, Down ]
-
-                    else if checkDown direction point.y then
-                        [ Right, Down ]
-
-                    else if checkUp direction point.y then
-                        [ Right, Up ]
-
-                    else
-                        [ Right ]
-
-                Down ->
-                    if checkDown direction (point.y + 2) && checkUp direction (point.y + 2) then
-                        [ Right, Up, Down ]
-
-                    else if checkDown direction (point.y + 2) then
-                        [ Right, Down ]
-
-                    else if checkUp direction (point.y + 2) then
-                        [ Right, Up ]
-
-                    else
-                        [ Right ]
-
-                Up ->
-                    if checkDown direction (point.y - 2) && checkUp direction (point.y - 2) then
-                        [ Right, Up, Down ]
-
-                    else if checkDown direction (point.y - 2) then
-                        [ Right, Down ]
-
-                    else if checkUp direction (point.y - 2) then
-                        [ Right, Up ]
-
-                    else
-                        [ Right ]
+        checkDown p =
+            (p.y + 2) < Area.heightTiles - 1
     in
-    case List.head path of
-        Nothing ->
-            [ Right, Up, Down ]
+    case direction of
+        Right ->
+            if checkDown point && checkUp point then
+                [ Right, Up, Down ]
 
-        Just pathPoint ->
-            internal pathPoint
+            else if checkDown point then
+                [ Right, Down ]
+
+            else if checkUp point then
+                [ Right, Up ]
+
+            else
+                [ Right ]
+
+        Down ->
+            if checkDown point then
+                [ Right, Down ]
+
+            else
+                [ Right ]
+
+        Up ->
+            if checkUp point then
+                [ Right, Up ]
+
+            else
+                [ Right ]
+
+
+generateRandomDirection : PathPoint -> Cmd Msg
+generateRandomDirection pathPoint =
+    pathPoint
+        |> checkDirection
+        |> Path.directionGenerator
+        |> Random.generate PathDirectionGenerate
 
 
 update : Msg -> Model -> ( Model, Cmd Messages.Msg )
 update msg model =
-    let
-        newPath pathPoint path direction =
-            case pathPoint.direction of
-                Right ->
-                    PathPoint (Point (pathPoint.point.x + 2) pathPoint.point.y) direction :: PathPoint (Point (pathPoint.point.x + 1) pathPoint.point.y) Right :: pathPoint :: path
-
-                Up ->
-                    PathPoint (Point pathPoint.point.x (pathPoint.point.y - 2)) direction :: PathPoint (Point pathPoint.point.x (pathPoint.point.y - 1)) Up :: pathPoint :: path
-
-                Down ->
-                    PathPoint (Point pathPoint.point.x (pathPoint.point.y + 2)) direction :: PathPoint (Point pathPoint.point.x (pathPoint.point.y + 1)) Down :: pathPoint :: path
-    in
     case model.gameState of
         Running ->
             ( model, Cmd.none )
@@ -123,14 +83,78 @@ update msg model =
                                     ( model, Cmd.none )
 
                                 h :: hs ->
-                                    if h.point.x + 2 > Area.widthTiles then
-                                        ( { model | gameState = Paused, path = Just (PathPoint (Point (h.point.x + 2) h.point.y) direction :: newPath h hs direction) }, Cmd.none )
+                                    case direction of
+                                        Right ->
+                                            if h.point.x + 1 >= Area.widthTiles - 1 then
+                                                if h.point.x >= Area.widthTiles - 1 then
+                                                    ( { model | gameState = Paused, path = Just (List.reverse ({ h | direction = direction } :: hs)) }, Cmd.none )
 
-                                    else
-                                        ( { model | path = Just (newPath h hs direction) }, Random.generate PathDirectionGenerate (Path.directionGenerator (checkDirection (newPath h hs direction))) )
+                                                else
+                                                    ( { model
+                                                        | gameState = Paused
+                                                        , path =
+                                                            Just
+                                                                (List.reverse
+                                                                    (PathPoint (Point (h.point.x + 1) h.point.y) direction
+                                                                        :: { h | direction = direction }
+                                                                        :: hs
+                                                                    )
+                                                                )
+                                                      }
+                                                    , Cmd.none
+                                                    )
+
+                                            else
+                                                ( { model
+                                                    | path =
+                                                        Just
+                                                            (PathPoint (Point (h.point.x + 2) h.point.y) direction
+                                                                :: PathPoint (Point (h.point.x + 1) h.point.y) direction
+                                                                :: { h | direction = direction }
+                                                                :: hs
+                                                            )
+                                                  }
+                                                , generateRandomDirection (PathPoint (Point (h.point.x + 2) h.point.y) direction)
+                                                )
+
+                                        Up ->
+                                            ( { model
+                                                | path =
+                                                    Just
+                                                        (PathPoint (Point h.point.x (h.point.y - 2)) direction
+                                                            :: PathPoint (Point h.point.x (h.point.y - 1)) direction
+                                                            :: { h | direction = direction }
+                                                            :: hs
+                                                        )
+                                              }
+                                            , generateRandomDirection (PathPoint (Point h.point.x (h.point.y - 2)) direction)
+                                            )
+
+                                        Down ->
+                                            ( { model
+                                                | path =
+                                                    Just
+                                                        (PathPoint (Point h.point.x (h.point.y + 2)) direction
+                                                            :: PathPoint (Point h.point.x (h.point.y + 1)) direction
+                                                            :: { h | direction = direction }
+                                                            :: hs
+                                                        )
+                                              }
+                                            , generateRandomDirection (PathPoint (Point h.point.x (h.point.y + 2)) direction)
+                                            )
 
                 PathPointGenerate point ->
-                    ( { model | path = Just [ PathPoint point Right ], gameState = GeneratePath }, Random.generate PathDirectionGenerate (Path.directionGenerator (checkDirection [ PathPoint point Right ])) )
+                    ( { model
+                        | path =
+                            Just
+                                [ PathPoint (Point (point.x + 2) point.y) Right
+                                , PathPoint (Point (point.x + 1) point.y) Right
+                                , PathPoint point Right
+                                ]
+                        , gameState = GeneratePath
+                      }
+                    , generateRandomDirection (PathPoint (Point (point.x + 2) point.y) Right)
+                    )
 
                 _ ->
                     ( model, Cmd.none )
