f9d8e483c99b24189d839965ff698c5537ce59e5
Cedrik Hoffmann <cedrik.hoffmann@jgdperl.com>
2022-08-29 14:58:46 +0200

    Remove TODO

--- src/Ui/Enemy.elm
+++ src/Ui/Enemy.elm
@@ -32,10 +32,6 @@ enemiesToCanvas enemies path =
                 |> Canvas.shapes [ Canvas.Settings.fill (Color.rgb255 50 255 50) ]
 
 
-
--- TODO: Not working
-
-
 renderEnemyIso : List Enemy -> Maybe Path -> Texture -> List Renderable
 renderEnemyIso enemies maybePath texture =
     case maybePath of
